3046cf087340ae69d89003d4c88993197c4f25c7
Zia <zia_gt@yahoo.com>
2025-03-03 21:42:15 +0100

    boilerplate-express

---
+++ myApp.js
@@ -0,0 +1,41 @@
+let express = require('express');
+console.log()
+console.log('Hello World')
+let app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
